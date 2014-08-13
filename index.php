1218a8057d0b44273f32dd6d0d1dee500700856a
teknogeek <jmargolis97@gmail.com>
2014-08-13 19:51:56 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+    <form method="POST" action="getsnaps.php">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
+        <input type="submit">
+    </form>
+</body>
+</html>
